21cdf3a6b633c8a3a45f0643acee6afbe6ac3cd1
Sandy Pratama <158142351+A5tama@users.noreply.github.com>
2024-05-04 18:27:34 +0700

    feat: landing-page & navbar (#6)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
+import Navigate from '@/components/Navigate';
+import Footer  from '@/components/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
+        <Navigate />
         {children}
         <Footer />
       </body>
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,95 +1,70 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+const home = () => {
+  const events = [
+    {
+      id: 1,
+      title: "Event 1",
+      description:
+        "Description of Event 1 :Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi maxime possimus unde impedit suscipit.",
+    },
+    {
+      id: 2,
+      title: "Event 2",
+      description:
+        "Description of Event 2 :Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi maxime possimus unde impedit suscipit.",
+    },
+    {
+      id: 3,
+      title: "Event 3",
+      description:
+        "Description of Event 3 :Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi maxime possimus unde impedit suscipit.",
+    },
+    {
+      id: 4,
+      title: "Event 4",
+      description:
+        "Description of Event 4 :Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi maxime possimus unde impedit suscipit.",
+    },
+    {
+      id: 5,
+      title: "Event 5",
+      description:
+        "Description of Event 5 :Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi maxime possimus unde impedit suscipit.",
+    },
+    {
+      id: 6,
+      title: "Event 6",
+      description:
+        "Description of Event 6 :Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi maxime possimus unde impedit suscipit.",
+    },
+    // Add more events as needed
+  ];
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="mt-5 text-3xl font-bold mb-8">Upcoming Events</h1>
+      <div className="grid grid-cols-1 w-screen p-4 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {events.map((event) => (
+          <div key={event.id} className="bg-white p-4 rounded shadow-md">
+            <div>
+              <Image
+                className="mx-auto"
+                width={300}
+                height={150}
+                src="/"
+                alt=""
+              />
+            </div>
+            <h2 className="text-xl font-semibold mb-2">{event.title}</h2>
+            <p className="text-gray-700">{event.description}</p>
+            <button className="bg-blue-500 text-white font-semibold px-4 py-2 mt-4 rounded hover:bg-blue-600">Buy Ticket</button>
+          </div>
+        ))}
       </div>
+    </div>
+  );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default home;
